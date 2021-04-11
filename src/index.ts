da49ec66da3512809316338669f0814aa717a7ac
bakjonghun <bakjonghun@hikick.kr>
2021-04-12 01:21:56 +0900

    add: sentry setup

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ import getRouter from './routes';
 import serverless from 'serverless-http';
 
 Database.initPrisma();
-export const handler = serverless(getRouter());
+const options = { basePath: '/v1/insurance' };
+export const handler = serverless(getRouter(), options);
