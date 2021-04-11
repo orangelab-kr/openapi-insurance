46589ce493687ea82e9aa590763ab507e1c2868e
bakjonghun <bakjonghun@hikick.kr>
2021-04-11 21:43:36 +0900

    add: database & error & wrapper

--- src/tools/wrapper.ts
+++ src/tools/wrapper.ts
@@ -1,10 +1,12 @@
+import * as Sentry from '@sentry/node';
+
 import { NextFunction, Request, Response } from 'express';
 
-import InternalError from './error';
 import OPCODE from './opcode';
+import { ValidationError } from 'joi';
 import logger from './logger';
 
-type Callback = (
+export type Callback = (
   req: Request,
   res: Response,
   next: NextFunction
@@ -21,16 +23,28 @@ export default function Wrapper(cb: Callback): Callback {
       }
 
       let status = 500;
+      let opcode = OPCODE.ERROR;
       let message = '알 수 없는 오류가 발생했습니다.';
+      const eventId = Sentry.captureException(err);
+      let details;
 
-      if (err instanceof InternalError) {
-        status = err.status;
+      if (err.name === 'InternalError') {
+        opcode = err.opcode;
         message = err.message;
+        details = err.details;
+      }
+
+      if (err instanceof ValidationError) {
+        status = 400;
+        message = '올바른 정보를 입력해주세요.';
+        details = err.details;
       }
 
       res.status(status).json({
-        opcode: OPCODE.ERROR,
+        opcode,
+        eventId,
         message,
+        details,
       });
     }
   };
